96b93d3cc8feb41f6801797b55dff6c11534431c
Ed Morley <501702+edmorley@users.noreply.github.com>
2022-11-17 13:54:16 +0000

    Make DataAPI's session optional
    And reorder some arguments/methods to make the diff for
    the implementation PR easier to read.

--- salesforce_functions/data_api/__init__.py
+++ salesforce_functions/data_api/__init__.py
@@ -9,16 +9,16 @@ from .record import Record, RecordModificationResult, RecordQueryResult
 class DataAPI:
     def __init__(
         self,
-        org_domain_url: str,
         api_version: str,
+        org_domain_url: str,
         access_token: str,
-        # TODO: Should we try and prevent this type from leaking into public API?
-        session: ClientSession,
+        session: ClientSession | None = None,
     ):
-        self.org_domain_url = org_domain_url
-        self.api_version = api_version
+        self._api_version = api_version
+        self._org_domain_url = org_domain_url
+        self._shared_session = session
+
         self.access_token = access_token
-        self._session = session
 
     async def query(self, soql: str) -> RecordQueryResult:
         """Queries for records with the given SOQL string."""
@@ -30,40 +30,6 @@ class DataAPI:
             f"{self.org_domain_url}/services/data/v{self.api_version}/query/",
             params={"q": soql},
         )
-        return result
-
-    async def _request(
-        self,
-        method: Literal["GET", "POST", "PATCH", "DELETE"],
-        url: str,
-        params: Mapping[str, str] | None = None,
-        json: Any | None = None,
-    ) -> Any:
-        # TODO: Set `timeout=N` here? (Default is 5 mins)
-        # TODO: Handle failure modes:
-        # cannot connect / timeout
-        # HTTP 5xx
-        # HTTP 4xx
-        # HTTP 3xx
-        # Not valid JSON
-        # [{"message":"A query string has to be specified","errorCode":"MALFORMED_QUERY"}]
-        # [{"message":"SOQL statements cannot be empty or null","errorCode":"MALFORMED_QUERY"}]
-        # [{"message":"INVALID_HEADER_TYPE","errorCode":"INVALID_AUTH_HEADER"}]
-        # [{"message":"Session expired or invalid","errorCode":"INVALID_SESSION_ID"}]
-        # [{"errorCode":"NOT_FOUND","message":"The requested resource does not exist"}]
-
-        response = await self._session.request(
-            method,
-            url,
-            headers={
-                "Authorization": f"Bearer {self.access_token}",
-                "Sforce-Call-Options": f"client=sf-functions-python:{__version__}",
-            },
-            params=params,
-            json=json,
-        )
-        # response.raise_for_status()
-        result = await response.json()
         return result
 
     async def query_more(
@@ -85,3 +51,46 @@ class DataAPI:
     ) -> RecordModificationResult:
         """Deletes a record, based on the given type and id."""
         raise NotImplementedError
+
+    async def _request(
+        self,
+        method: Literal["GET", "POST", "PATCH", "DELETE"],
+        url: str,
+        params: Mapping[str, str] | None = None,
+        json: Any | None = None,
+    ) -> Any:
+        # TODO: Set `timeout=N` here? (Default is 5 mins)
+        # TODO: Handle failure modes:
+        # cannot connect / timeout
+        # HTTP 5xx
+        # HTTP 4xx
+        # HTTP 3xx
+        # Not valid JSON
+        # [{"message":"A query string has to be specified","errorCode":"MALFORMED_QUERY"}]
+        # [{"message":"SOQL statements cannot be empty or null","errorCode":"MALFORMED_QUERY"}]
+        # [{"message":"INVALID_HEADER_TYPE","errorCode":"INVALID_AUTH_HEADER"}]
+        # [{"message":"Session expired or invalid","errorCode":"INVALID_SESSION_ID"}]
+        # [{"errorCode":"NOT_FOUND","message":"The requested resource does not exist"}]
+
+        headers = {
+            "Authorization": f"Bearer {self.access_token}",
+            "Sforce-Call-Options": f"client=sf-functions-python:{__version__}",
+        }
+
+        session = self._shared_session or ClientSession()
+
+        try:
+            response = await session.request(
+                method,
+                url,
+                headers=headers,
+                params=params,
+                json=json,
+            )
+            # response.raise_for_status()
+            result = await response.json()
+        finally:
+            if not self._shared_session:
+                await session.close()
+
+        return result
